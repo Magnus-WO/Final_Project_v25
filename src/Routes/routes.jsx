d2e43eb5a922622798d485ce31cb8ded20aa1f5b
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2025-05-19 11:12:31 +0200

    create router structure

---
+++ src/Routes/routes.jsx
@@ -0,0 +1,27 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+// Import pages
+import App from "../App";
+import Home from "../Pages/Home/Home";
+import Merch from "../Pages/Merch/Merch";
+import SignIn from "../Pages/SignIn/SignIn";
+import SignUp from "../Pages/SignUp/SignUp";
+import Contact from "../Pages/Contact/Contact";
+import NotFound from "../Pages/NotFound/NotFound";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="/merch" element={<Merch />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
