713bec7facef6ee375680542ed50a5ccd5aeef5a
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2025-06-04 21:48:03 +0200

    add notFoundPage

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./NotFound.module.css";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div></div>;
+  return (
+    <section className={styles.pageNotFound}>
+      <p>The page you searched for doesnt exist</p>
+      <Link to="/">Cick here to go to the homepage</Link>
+    </section>
+  );
 };
 
 export default NotFound;
